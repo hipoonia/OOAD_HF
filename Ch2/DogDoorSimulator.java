060040531bd367081178ab79d28f12840b0aa873
hipoonia <prdp295@gmail.com>
2019-06-14 18:48:22 +0530

    Dog Door V3- alternate path

--- Ch2/DogDoorSimulator.java
+++ Ch2/DogDoorSimulator.java
@@ -8,13 +8,26 @@ public class DogDoorSimulator {
 		System.out.println("Pillu barks to go out...");
 		remote.pressButton();
 		
+		
 		System.out.println("\n Pillu has gnoe out...");
 	//	remote.pressButton();
 		
 		System.out.println("\n Pillu's all done...");
 	//	remote.pressButton();
 		
-		System.out.println("\n Pillu is back inside...");
+		
+		try {
+		
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e) {} 
+		
+		System.out.println("\n But pillu is stuck outside...");
+		System.out.println("\n pillu starts barking again!...");
+		
+		System.out.println("\n User grabs the remote again...");
+		remote.pressButton();
+		
+		System.out.println("\n Finally Pillu is back inside...");
 	//	remote.pressButton();
 	}
 }
