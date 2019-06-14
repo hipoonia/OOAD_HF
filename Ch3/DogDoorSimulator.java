35f365e70e3bf53db4c183cea2d661251e75f8cb
hipoonia <prdp295@gmail.com>
2019-06-14 20:12:53 +0530

    DogDoor v3 BarkRecognizer

---
+++ Ch3/DogDoorSimulator.java
@@ -0,0 +1,38 @@
+public class DogDoorSimulator {
+
+	public static void main (String[] args) {
+	
+		DogDoor door = new DogDoor();
+		
+		BarkRecognizer recognizer = new BarkRecognizer(door);
+		
+		Remote remote = new Remote(door);
+		
+		System.out.println("Pillu barks to go out...");
+	//	remote.pressButton();
+		recognizer.recognize("Woof");
+		
+		System.out.println("\n Pillu has gone out...");
+	//	remote.pressButton();
+		
+		System.out.println("\n Pillu's all done...");
+	//	remote.pressButton();
+		
+		
+		try {
+		
+			Thread.currentThread().sleep(10000);
+		} catch (InterruptedException e) {} 
+		
+		System.out.println("\n But pillu is stuck outside...");
+		System.out.println("\n pillu starts barking again!...");
+		
+	//	System.out.println("\n User grabs the remote again...");
+	//	remote.pressButton();
+		
+		recognizer.recognize("Woof");
+		
+		System.out.println("\n Finally Pillu is back inside...");
+	//	remote.pressButton();
+	}
+}
