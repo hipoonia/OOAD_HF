76a27e5cfab2e2a3fd840fe40672b26b601be726
hipoonia <prdp295@gmail.com>
2019-06-17 19:24:14 +0530

    Mandolin

---
+++ Ch5/InstrumentSpec.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public abstract class InstrumentSpec {
+	
+	private Builder builder;
+	private String model;
+	private Type type;
+	private Wood backWood;
+	private Wood topWood;
+	
+	public InstrumentSpec (Builder builder, String model, Type type, Wood backWood, Wood topWood) {
+		
+		this.builder = builder;
+		this.model = model;
+		this.type = type;
+		this.backWood = backWood;
+		this.topWood = topWood;
+	}
+	
+	// getter methods for builder model type etc
+	
+	public Builder getBuilder() {
+	
+		return builder;
+	}
+	
+	public Type getType() {
+	
+		return type;
+	}
+	
+	public Wood getBackWood() {
+	
+		return backWood;
+	}
+	
+	public Wood getTopWood() {
+	
+		return topWood;
+	}
+	
+	public String getModel() {
+	
+		return model;
+	}
+	
+	public boolean matches(InstrumentSpec otherSpec) {
+		
+		if (builder != otherSpec.builder) return false;
+		
+		if ((model!=null) && (!model.equals("")) && (!model.equals(otherSpec.model))) return false;
+		
+		if (type!=otherSpec.type) return false;
+		
+		if (backWood!=otherSpec.backWood) return false;
+		
+		if (topWood!=otherSpec.topWood) return false;
+		
+		return true;
+		
+	}
+	
+}
